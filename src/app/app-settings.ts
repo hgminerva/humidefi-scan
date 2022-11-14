81f90657f7aee9209f8fc1e47e7dcb841f6c5599
hgminerva <hgminerva@gmail.com>
2022-11-14 12:05:26 +0800

    Endpoints updated to testnet

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,10 +1,10 @@
 export class AppSettings {
-    public wsProviderEndpoint = localStorage.getItem('network') == 'Mainnet' ? 'ws://127.0.0.1:9948' : 
-        localStorage.getItem('network') == 'Testnet' ? 'ws://127.0.0.1:9948' : 
-        localStorage.getItem('network') == 'Local' ? 'ws://127.0.0.1:9948' : 
+    public wsProviderEndpoint = localStorage.getItem('network') == 'Local' ? 'ws://127.0.0.1:8844' : 
+        localStorage.getItem('network') == 'Testnet' ? 'wss://bootnode001.humidefi.com' : 
+        localStorage.getItem('network') == 'Mainnet' ? 'wss://bootnode001.humidefi.com' : 
         localStorage.getItem('network') == 'Polkadot' ? 'wss://rpc.polkadot.io' : 
         localStorage.getItem('network') == 'Astar' ? 'wss://astar.public.blastapi.io' :
-        localStorage.getItem('network') == 'Acala' ? 'wss://acala-rpc.dwellir.com' : 'ws://127.0.0.1:9948';
+        localStorage.getItem('network') == 'Acala' ? 'wss://acala-rpc.dwellir.com' : 'wss://bootnode001.humidefi.com';
 
     public keypair = localStorage.getItem("wallet-keypair") || "";
     public phpuContractAddress = localStorage.getItem("phpu-contract-address") || "";
